8ee80900c2e9531d1a27917221b8189f63adf19e
luisrm <luisrmmalaga@gmail.com>
2022-09-15 19:45:59 +0200

    Title from API docs changed

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from routes.usuario import user
 from routes.logUsuario import logUser
 
-app = FastAPI(tittle="PCM REST API with FastAPI and MongoDB", version= "0.0.1")
+app = FastAPI(title="PCM REST API with FastAPI and MongoDB", version= "0.0.1")
 
 app.include_router(user)
 app.include_router(logUser)
